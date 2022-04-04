11d19339ee9816b102a8c38f1a82bbaa9014eae6
Sasha2703 <saschapanasuk2703@gmail.com>
2022-04-04 23:24:47 +0300

    Finish.

--- web/modules/custom/guestbook/src/Controller/GuestbookController.php
+++ web/modules/custom/guestbook/src/Controller/GuestbookController.php
@@ -13,71 +13,81 @@ use Drupal\file\Entity\File;
 /**
  * Provides content.
  */
-class GuestbookController extends ControllerBase
-{
+class GuestbookController extends ControllerBase {
 
-    /**
-     * Implements content().
-     */
-    public function content()
-    {
-        $guestbook['book_form'] = \Drupal::formBuilder()->getForm('Drupal\guestbook\Form\BookForm');
-        $query = \Drupal::database()->select('responses', 'r');
-        $query->fields('r', ['id', 'author_name', 'email', 'phone', 'avatar', 'image,', 'message', 'timestamp'])->orderBy('timestamp', 'desc');
-        $data = $query->execute()->fetchAll();
-        $responses = [];
+  /**
+   * Implements content().
+   */
+  public function content() {
+    $guestbook['book_form'] = \Drupal::formBuilder()
+      ->getForm('Drupal\guestbook\Form\BookForm');
+    $query = \Drupal::database()->select('responses', 'r');
+    $query->fields('r', [
+      'id',
+      'author_name',
+      'email',
+      'phone',
+      'avatar',
+      'image,',
+      'message',
+      'timestamp',
+    ])->orderBy('timestamp', 'desc');
+    $data = $query->execute()->fetchAll();
+    $responses = [];
 
-        foreach ($data as $field) {
-            if (!$field->avatar == 0) {
-                $file_avatar = File::load($field->avatar);
-                $avatar_uri = $file_avatar->getFileUri();
-                $avatar_is_set = true;
-            } else {
-                $avatar_uri = '/modules/custom/guestbook/images/default_user.png';
-                $avatar_is_set = false;
-            }
-            if (!$field->image == 0) {
-                $file_image = File::load($field->image);
-                $image_uri = $file_image->getFileUri();
-            } else {
-                $image_uri = 0;
-            }
+    foreach ($data as $field) {
+      if (!$field->avatar == 0) {
+        $file_avatar = File::load($field->avatar);
+        $avatar_uri = $file_avatar->getFileUri();
+        $avatar_is_set = TRUE;
+      }
+      else {
+        $avatar_uri = '/modules/custom/guestbook/images/default_user.png';
+        $avatar_is_set = FALSE;
+      }
+      if (!$field->image == 0) {
+        $file_image = File::load($field->image);
+        $image_uri = $file_image->getFileUri();
+      }
+      else {
+        $image_uri = 0;
+      }
 
-            $avatar_img = [
-                '#theme' => 'image_style',
-                '#style_name' => 'wide',
-                '#uri' => $avatar_uri,
-                '#title' => 'avatar',
-                '#width' => 50,
-                '#height' => 50,
-                '#isset' => $avatar_is_set,
-            ];
-            $image_img = [
-                '#theme' => 'image_style',
-                '#style_name' => 'wide',
-                '#uri' => $image_uri,
-                '#title' => 'image',
-                '#width' => 150,
-                '#height' => 150,
-            ];
-            $responses[] = [
-                'id' => $field->id,
-                'name' => $field->author_name,
-                'email' => $field->email,
-                'phone' => $field->phone,
-                'message' => $field->message,
-                'avatar' => $avatar_img,
-                'image' => $image_img,
-                'created_time' => $field->timestamp,
-            ];
-        }
-
-        $build = [
-            '#theme' => 'guestbook-template',
-            '#responses' => $responses,
-            '#form' => $guestbook,
-        ];
-        return $build;
+      $avatar_img = [
+        '#theme' => 'image_style',
+        '#style_name' => 'wide',
+        '#uri' => $avatar_uri,
+        '#title' => 'avatar',
+        '#width' => 50,
+        '#height' => 50,
+        '#isset' => $avatar_is_set,
+      ];
+      $image_img = [
+        '#theme' => 'image_style',
+        '#style_name' => 'wide',
+        '#uri' => $image_uri,
+        '#title' => 'image',
+        '#width' => 150,
+        '#height' => 150,
+      ];
+      $responses[] = [
+        'id' => $field->id,
+        'name' => $field->author_name,
+        'email' => $field->email,
+        'phone' => $field->phone,
+        'message' => $field->message,
+        'avatar' => $avatar_img,
+        'image' => $image_img,
+        'created_time' => $field->timestamp,
+      ];
     }
+
+    $build = [
+      '#theme' => 'guestbook-template',
+      '#responses' => $responses,
+      '#form' => $guestbook,
+    ];
+    return $build;
+  }
 
 }
--- web/modules/custom/guestbook/src/Form/DeleteBookForm.php
+++ web/modules/custom/guestbook/src/Form/DeleteBookForm.php
@@ -14,85 +14,75 @@ use Drupal\Core\Url;
 /**
  * Provides content.
  */
-class DeleteBookForm extends ConfirmFormBase
-{
+class DeleteBookForm extends ConfirmFormBase {
 
-    /**
-     * Return Question.
-     */
-    public function getQuestion()
-    {
-        return $this->t('Do you want to delete this response?');
-    }
+  /**
+   * Return Question.
+   */
+  public function getQuestion() {
+    return $this->t('Do you want to delete this response?');
+  }
 
-    /**
-     * Return URL if cancel.
-     */
-    public function getCancelUrl()
-    {
-        return new Url('guestbook.content');
-    }
+  /**
+   * Return URL if cancel.
+   */
+  public function getCancelUrl() {
+    return new Url('guestbook.content');
+  }
 
-    /**
-     * Return Description.
-     */
-    public function getDescription()
-    {
-        return $this->t('Do you want to delete?');
-    }
+  /**
+   * Return Description.
+   */
+  public function getDescription() {
+    return $this->t('Do you want to delete?');
+  }
 
-    /**
-     * Return confirm text.
-     */
-    public function getConfirmText()
-    {
-        return $this->t('Delete');
-    }
+  /**
+   * Return confirm text.
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
 
-    /**
-     * Return cancel text.
-     */
-    public function getCancelText()
-    {
-        return t('Cancel');
-    }
+  /**
+   * Return cancel text.
+   */
+  public function getCancelText() {
+    return t('Cancel');
+  }
 
-    /**
-     * Return FormID.
-     */
-    public function getFormId()
-    {
-        return 'Delete Response';
-    }
+  /**
+   * Return FormID.
+   */
+  public function getFormId() {
+    return 'Delete Response';
+  }
 
-    /**
-     * Return form.
-     */
-    public function buildForm(array $form, FormStateInterface $form_state, $bookID = null)
-    {
-        $this->id = $bookID;
-        return parent::buildForm($form, $form_state);
-    }
+  /**
+   * Return form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $bookID = NULL) {
+    $this->id = $bookID;
+    return parent::buildForm($form, $form_state);
+  }
 
-    /**
-     * Validate form.
-     */
-    public function validateForm(array &$form, FormStateInterface $form_state)
-    {
-        parent::validateForm($form, $form_state);
-    }
+  /**
+   * Validate form.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
 
-    /**
-     * Submit form.
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
-        $query = \Drupal::database();
-        $query->delete('responses')
-            ->condition('id', $this->id)
-            ->execute();
-        \Drupal::messenger()->addStatus('You deleted the response');
-        $form_state->setRedirect('guestbook.content');
-    }
+  /**
+   * Submit form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $query = \Drupal::database();
+    $query->delete('responses')
+      ->condition('id', $this->id)
+      ->execute();
+    \Drupal::messenger()->addStatus('You deleted the response');
+    $form_state->setRedirect('guestbook.content');
+  }
 
 }
